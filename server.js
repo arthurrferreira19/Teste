b6ed4232a0b183b5fcaf5e49576443d8468f505b
arthurrferreira19 <programacao@maximumcontabil.com.br>
2026-02-19 11:58:08 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const { connectDB } = require("./src/config/db");
 const app = require("./src/app");
 const { ensureAdminSeed } = require("./src/services/seedService");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => console.log("Running on", PORT));
-
 (async () => {
   try {
     await connectDB();
     await ensureAdminSeed();
 
-    app.listen(PORT, () => {
-      console.log(`[server] rodando na porta ${PORT}`);
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Running on", PORT);
     });
   } catch (err) {
     console.error("[boot] falha ao iniciar:", err);
